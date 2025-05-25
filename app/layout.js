58aab6bfa2d0e7676acc7004d498abeea3e18fbe
Dmitry556 <dmytropinchuk1@gmail.com>
2025-05-25 05:09:46 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'VeoGrowth - Free B2B Cold Email Campaign Generator',
+  description: 'Generate hyper-personalized cold email campaigns in 20 seconds',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
